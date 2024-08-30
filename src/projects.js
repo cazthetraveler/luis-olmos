0358f942d54d9447c1eec692becb9095ade40913
MultiMouse <olmos.luisd@outlook.com>
2024-08-29 19:52:36 -0500

    added luis-photo project

--- src/projects.js
+++ src/projects.js
@@ -1,101 +1,173 @@
 const projects = [
-    {
-        id: 1,
-        category: 'graphic-design',
-        title: 'Coastal Interiors Logo Design',
-        description: 'A logo design project for an interior company. This logo utilizes a modern/minimalist style.',
-        thumb: {
-            src: '/images/gd/coastal-logo/coastal-logo-thumb.png',
-            alt: 'Logo Design project for an interior company.'
-        },
-        link: '',
-        image: [
-            {
-                src: '/images/gd/coastal-logo/case-study-01.png',
-                alt: 'case study image',
-                class: 'square'
-            },
-            {
-                src: '/images/gd/coastal-logo/case-study-02.png',
-                alt: 'case study image',
-                class: 'square'
-            },
-            {
-                src: '/images/gd/coastal-logo/case-study-03.png',
-                alt: 'case study image',
-                class: 'square'
-            },
-            {
-                src: '/images/gd/coastal-logo/case-study-04.png',
-                alt: 'case study image',
-                class: 'square'
-            },
-            {
-                src: '/images/gd/coastal-logo/case-study-05.png',
-                alt: 'case study image',
-                class: 'square'
-            },
-            {
-                src: '/images/gd/coastal-logo/case-study-06.png',
-                alt: 'case study image',
-                class: 'square'
-            },
-            {
-                src: '/images/gd/coastal-logo/case-study-07.png',
-                alt: 'case study image',
-                class: 'square'
-            },
-            {
-                src: '/images/gd/coastal-logo/case-study-08.png',
-                alt: 'case study image',
-                class: 'square'
-            },
-        ]
+  {
+    id: 1,
+    category: "graphic-design",
+    title: "Coastal Interiors Logo Design",
+    description:
+      "A logo design project for an interior company. This logo utilizes a modern/minimalist style.",
+    thumb: {
+      src: "/images/gd/coastal-logo/coastal-logo-thumb.png",
+      alt: "Logo Design project for an interior company.",
     },
-    {
-        id: 2,
-        category: 'web-development',
-        title: 'Weather Dashboard',
-        description: 'A web api project using DayJS and OpenWeatherMap.',
-        thumb: {
-            src: '/images/wd/weather-dashboard-thumb.png',
-            alt: 'Weather API project.'
-        },
-        link: 'https://github.com/cazthetraveler/Weather-Dashboard',
+    link: "",
+    image: [
+      {
+        src: "/images/gd/coastal-logo/case-study-01.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/coastal-logo/case-study-02.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/coastal-logo/case-study-03.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/coastal-logo/case-study-04.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/coastal-logo/case-study-05.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/coastal-logo/case-study-06.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/coastal-logo/case-study-07.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/coastal-logo/case-study-08.png",
+        alt: "case study image",
+        class: "square",
+      },
+    ],
+  },
+  {
+    id: 2,
+    category: "web-development",
+    title: "Weather Dashboard",
+    description: "A web api project using DayJS and OpenWeatherMap.",
+    thumb: {
+      src: "/images/wd/weather-dashboard-thumb.png",
+      alt: "Weather API project.",
     },
-    {
-        id: 3,
-        category: 'web-development',
-        title: 'weeklyMoodal',
-        description: 'A mood tracker utlizing web apis and local storage. Done in collaboration with Ryan Fann and James Wilson.',
-        thumb: {
-            src: '/images/wd/weekly-moodal-thumb.png',
-            alt: 'Mood Tracker project.'
-        },
-        link: 'https://github.com/jameslwilson364/weeklyMoodal',
+    link: "https://github.com/cazthetraveler/Weather-Dashboard",
+  },
+  {
+    id: 3,
+    category: "web-development",
+    title: "weeklyMoodal",
+    description:
+      "A mood tracker utlizing web apis and local storage. Done in collaboration with Ryan Fann and James Wilson.",
+    thumb: {
+      src: "/images/wd/weekly-moodal-thumb.png",
+      alt: "Mood Tracker project.",
     },
-    {
-        id: 4,
-        category: 'web-development',
-        title: 'Wizard Escape',
-        description: 'A survival escape game using MVC. Done in collaboration with Drew Casas and Evan Rice.',
-        thumb: {
-            src: '/images/wd/wizard-escape-thumb.png',
-            alt: 'A web game project.'
-        },
-        link: 'https://github.com/drewhouses/wizard-escape',
+    link: "https://github.com/jameslwilson364/weeklyMoodal",
+  },
+  {
+    id: 4,
+    category: "web-development",
+    title: "Wizard Escape",
+    description:
+      "A survival escape game using MVC. Done in collaboration with Drew Casas and Evan Rice.",
+    thumb: {
+      src: "/images/wd/wizard-escape-thumb.png",
+      alt: "A web game project.",
     },
-    {
-        id: 5,
-        category: 'web-development',
-        title: 'Bloom & Petal Mern Site',
-        description: 'A commerce site using the MERN Stack.',
-        thumb: {
-            src: '/images/wd/bloom-petal-mern-thumb.png',
-            alt: 'A commerce site mern project.'
-        },
-        link: 'https://github.com/cazthetraveler/Bloom-and-Petal-Floristry',
+    link: "https://github.com/drewhouses/wizard-escape",
+  },
+  {
+    id: 5,
+    category: "web-development",
+    title: "Bloom & Petal Mern Site",
+    description: "A commerce site using the MERN Stack.",
+    thumb: {
+      src: "/images/wd/bloom-petal-mern-thumb.png",
+      alt: "A commerce site mern project.",
     },
+    link: "https://github.com/cazthetraveler/Bloom-and-Petal-Floristry",
+  },
+  {
+    id: 1,
+    category: "graphic-design",
+    title: "Luis Olmos Photography Logo Design",
+    description:
+      "A logo design project for myself, as a photographer. This logo utilizes a modern/minimalist style.",
+    thumb: {
+      src: "/images/gd/luis-photo/luis-photo-thumb.png",
+      alt: "Logo Design project for myself.",
+    },
+    link: "",
+    image: [
+      {
+        src: "/images/gd/luis-photo/case-study-01.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/luis-photo/case-study-02.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/luis-photo/case-study-03.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/luis-photo/case-study-04.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/luis-photo/case-study-05.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/luis-photo/case-study-06.png",
+        alt: "case study image",
+        class: "landscape",
+      },
+      {
+        src: "/images/gd/luis-photo/case-study-07.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/luis-photo/case-study-08.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/luis-photo/case-study-09.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/luis-photo/case-study-10.png",
+        alt: "case study image",
+        class: "square",
+      },
+      {
+        src: "/images/gd/luis-photo/case-study-11.png",
+        alt: "case study image",
+        class: "square",
+      },
+    ],
+  },
 ];
 
 export default projects;
